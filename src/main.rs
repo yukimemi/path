2d00baa4f0520cda12e4b22f64ccbd9b9641d5d1
yukimemi <yukimemi@gmail.com>
2020-10-19 01:16:47 +0900

    :bug: Fix global flg.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,49 @@
 use anyhow::{Context, Result};
-use clap::{app_from_crate, App, AppSettings, ArgMatches};
+use clap::{app_from_crate, App, AppSettings, Arg, ArgMatches};
 use std::path::Path;
 
-fn extension(rm: &ArgMatches, m: &ArgMatches) -> Result<String> {
-    let path = rm.value_of("PATH").context("<PATH> is required !")?;
+fn extension(m: &ArgMatches) -> Result<String> {
+    let path = m.value_of("PATH").context("<PATH> is required !")?;
     let path = Path::new(&path);
     let extension = path.extension().context("Failed to get extension !")?;
-    println!("{:?}", &extension);
     Ok(extension.to_string_lossy().to_string())
 }
 
-fn parent(rm: &ArgMatches, m: &ArgMatches) -> Result<String> {
-    let path = rm.value_of("PATH").context("<PATH> is required !")?;
+fn parent(m: &ArgMatches) -> Result<String> {
+    let path = m.value_of("PATH").context("<PATH> is required !")?;
     let path = Path::new(&path);
     let parent = path.parent().context("Failed to get parent !")?;
-    println!("{:?}", &parent);
     Ok(parent.to_string_lossy().to_string())
 }
 
 fn main() -> Result<()> {
     let matches = app_from_crate!()
         .setting(AppSettings::DeriveDisplayOrder)
-        .arg("-n --newline 'Output a newline'")
-        .arg("<PATH>          'target path'")
-        .subcommand(App::new("parent").about("Get parent directory"))
-        .subcommand(App::new("extension").about("Get file extension"))
+        .setting(AppSettings::SubcommandRequired)
+        .arg(Arg::from("-n --newline 'Output a newline'").global(true))
+        .subcommand(
+            App::new("parent")
+                .about("Get parent directory")
+                .arg("<PATH>          'target path'"),
+        )
+        .subcommand(
+            App::new("extension")
+                .about("Get file extension")
+                .arg("<PATH>          'target path'"),
+        )
         .get_matches();
 
-    dbg!(&matches);
-
     let s = match matches.subcommand() {
-        Some(("parent", sub_m)) => parent(&matches, sub_m)?,
-        Some(("extension", sub_m)) => extension(&matches, sub_m)?,
-        _ => "".to_string(),
+        Some(("parent", sub_m)) => parent(sub_m)?,
+        Some(("extension", sub_m)) => extension(sub_m)?,
+        _ => unreachable!(),
     };
 
-    print!("{}", s);
+    if matches.is_present("newline") {
+        println!("{}", s);
+    } else {
+        print!("{}", s);
+    }
 
     Ok(())
 }
