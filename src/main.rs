3614c7878feb192b087603a6dbdea81c2e007f19
yukimemi <yukimemi@gmail.com>
2020-10-19 20:46:52 +0900

    :zap: Add file_name and file_stem subcommands.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,20 @@ fn parent(m: &ArgMatches) -> Result<String> {
     Ok(parent.to_string_lossy().to_string())
 }
 
+fn file_name(m: &ArgMatches) -> Result<String> {
+    let path = m.value_of("PATH").context("<PATH> is required !")?;
+    let path = Path::new(&path);
+    let file_name = path.file_name().context("Failed to get file_name")?;
+    Ok(file_name.to_string_lossy().to_string())
+}
+
+fn file_stem(m: &ArgMatches) -> Result<String> {
+    let path = m.value_of("PATH").context("<PATH> is required !")?;
+    let path = Path::new(&path);
+    let file_stem = path.file_stem().context("Failed to get file_stem")?;
+    Ok(file_stem.to_string_lossy().to_string())
+}
+
 fn main() -> Result<()> {
     let matches = app_from_crate!()
         .setting(AppSettings::DeriveDisplayOrder)
@@ -27,6 +41,16 @@ fn main() -> Result<()> {
                 .arg("<PATH>          'target path'"),
         )
         .subcommand(
+            App::new("file_name")
+                .about("Returns the final component of the path")
+                .arg("<PATH>          'target path'"),
+        )
+        .subcommand(
+            App::new("file_stem")
+                .about("Extracts the stem (non-extension) portion of file_name")
+                .arg("<PATH>          'target path'"),
+        )
+        .subcommand(
             App::new("extension")
                 .about("Get file extension")
                 .arg("<PATH>          'target path'"),
@@ -35,6 +59,8 @@ fn main() -> Result<()> {
 
     let s = match matches.subcommand() {
         Some(("parent", sub_m)) => parent(sub_m)?,
+        Some(("file_name", sub_m)) => file_name(sub_m)?,
+        Some(("file_stem", sub_m)) => file_stem(sub_m)?,
         Some(("extension", sub_m)) => extension(sub_m)?,
         _ => unreachable!(),
     };
